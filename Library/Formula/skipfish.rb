47bc75f31e7cf8d681dce97ad1bf8fa26c5fab44
Brett Koonce <koonce@gmail.com>
2012-11-18 19:35:44 -0600

    skipfish 2.09b

--- Library/Formula/skipfish.rb
+++ Library/Formula/skipfish.rb
@@ -2,20 +2,15 @@ require 'formula'
 
 class Skipfish < Formula
   homepage 'http://code.google.com/p/skipfish/'
-  url 'http://skipfish.googlecode.com/files/skipfish-2.07b.tgz'
-  sha1 'eb8811137dd474adc02a657d371c4427b28f8d97'
+  url 'http://skipfish.googlecode.com/files/skipfish-2.09b.tgz'
+  sha1 'c3c7c561e9e033b96bb7a915b6f6c9a27e8828dd'
 
   depends_on 'libidn'
 
   def install
     ENV.append "CFLAGS", "-I#{HOMEBREW_PREFIX}/include"
     ENV.append "LDFLAGS", "-L#{HOMEBREW_PREFIX}/lib"
-
-    chmod 0755, "config.h" # Not writeable in the tgz. Lame.
-    inreplace "config.h",
-      "#define ASSETS_DIR              \"assets\"",
-      "#define ASSETS_DIR	       \"#{libexec}/assets\""
-
+    
     system 'make'
     bin.install 'skipfish'
     libexec.install %w(assets dictionaries)
