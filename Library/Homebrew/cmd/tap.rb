751be666196e3e4ecc6b92992f714a5991eb62b1
CNA-Bld <cna.bld@gmail.com>
2015-06-02 20:44:55 +0800

    basic implementation of tap priority

--- Library/Homebrew/cmd/tap.rb
+++ Library/Homebrew/cmd/tap.rb
@@ -15,28 +15,57 @@ module Homebrew
 
   def install_tap user, repo, clone_target=nil
     tap = Tap.new user, repo
-    return false if tap.installed?
-    ohai "Tapping #{tap}"
-    remote = clone_target || "https://github.com/#{tap.user}/homebrew-#{tap.repo}"
-    args = %W[clone #{remote} #{tap.path}]
-    args << "--depth=1" unless ARGV.include?("--full")
-    safe_system "git", *args
 
-    formula_count = tap.formula_files.size
-    puts "Tapped #{formula_count} formula#{plural(formula_count, 'e')} (#{tap.path.abv})"
+    if tap.installed?
+      ohai "#{user}/#{repo} already tapped. Changing priority."
+      priority = ARGV.value("priority")
+      if priority.nil?
+        opoo "Priority not specified, terminating."
+      else
+        priority = priority.to_i
+        if priority < 0 or priority > 99
+          opoo "Priority not allowed, terminating."
+        else
+          tap.set_priority priority
+        end
+      end
+      true
+    else
+      ohai "Tapping #{tap}"
+      remote = clone_target || "https://github.com/#{tap.user}/homebrew-#{tap.repo}"
+      args = %W[clone #{remote} #{tap.path}]
+      args << "--depth=1" unless ARGV.include?("--full")
+      safe_system "git", *args
 
-    if !clone_target && tap.private?
-      puts <<-EOS.undent
+      formula_count = tap.formula_files.size
+      puts "Tapped #{formula_count} formula#{plural(formula_count, 'e')} (#{tap.path.abv})"
+
+      priority = ARGV.value("priority")
+      if priority.nil?
+        opoo "Priority not specified, default is 99."
+        priority = 99
+      else
+        priority = priority.to_i
+        if priority < 0 or priority > 99
+          opoo "Priority not allowed, default is 99."
+          priority = 99
+        end
+      end
+
+      tap.set_priority priority
+
+      if !clone_target && tap.private?
+        puts <<-EOS.undent
         It looks like you tapped a private repository. To avoid entering your
         credentials each time you update, you can use git HTTP credential
         caching or issue the following command:
 
           cd #{tap.path}
           git remote set-url origin git@github.com:#{tap.user}/homebrew-#{tap.repo}.git
-      EOS
+        EOS
+      end
+      true
     end
-
-    true
   end
 
   # Migrate tapped formulae from symlink-based to directory-based structure.
